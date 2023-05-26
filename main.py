dc73aeb4db6e6d39b20d6f74f617b005af6a2de6
adhitht <adhith.tharammal@gmail.com>
2023-05-26 12:28:57 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,19 @@
+ll = '''<li>Pranjali Verma (2022-23)</li>
+                    <li>Malay Ghosh (2022-23)</li>
+                    <li>Subhankar Sasmal (2022-23)</li>
+                    <li>Amit Ranjan (2021-22)</li>
+                    <li>Tanmoy Mondal (2021-22)</li>
+                    <li>Shobhit Mathur (2021-22)</li>
+                    <li>Debolina Saha (2020-21)</li>
+                    <li>Aanshi Jain (2020-21)</li>
+                    <li>Biplab Mahata (2019-20)</li>
+                    <li>Manav Chauhan (2019-20)</li>
+                    <li>Jaya Yadav (2018-19)</li>
+                    <li>Sraddha Gupta (2018-19)</li>
+                    <li>Jyotirmoy Halder (2017-18)</li>
+                    <li>Paras (2017-18)</li>
+                    <li>Rahul Shukla (2017-18)</li>
+                    <li>Lalita Tanwar (2016-17)</li>
+                    <li>Arabinda Muley (2016-17)</li>'''
+kl = ll.split('\n')[::-1]
+print("\n".join(kl))
